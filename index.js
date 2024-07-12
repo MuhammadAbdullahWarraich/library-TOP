de85adf4b6ad4dbb8d18252bdbfa21cb1d024d1d
MuhammadAbdullahWarraich <muhammadabdullah2016pk@gmail.com>
2024-07-12 18:12:55 +0500

    feat: add rendering + add/remove functionality

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ closeBookForm.addEventListener('click', () =>{
 
 function addBookToLibrary(book) {
     myLibrary.push(new Book(book.title, book.author, book.noOfPages, book.haveIRead));
+    renderBook(myLibrary.length - 1);
     console.log(myLibrary);
 }
 
@@ -44,3 +45,73 @@ bookForm.addEventListener("submit", (e) =>{
     bookForm.classList.remove('is-open');
     e.preventDefault();
 });
+
+function toggleReadStatus(button) {
+    if (button.classList.contains('have-read-this')) {
+        button.classList.remove('have-read-this');
+        button.classList.add('not-read-yet');
+        button.innerHTML = "Not Read Yet";
+    } else {
+        button.classList.remove('not-read-yet');
+        button.classList.add('have-read-this');
+        button.innerHTML = "Have Read This";
+    }
+}
+
+function removeFromLibrary(bookDiv) {
+    bookDiv.remove();
+    let i = myLibrary.indexOf(bookDiv);
+    if (-1 !== i) {
+        myLibrary.splice(i, 1);
+        const emptyMessage = document.querySelector("#no-books");
+        emptyMessage.style.display = "block";
+    }
+}
+
+function renderBook(i) {
+    const emptyMessage = document.querySelector("#no-books");
+    emptyMessage.style.display = "none";
+
+    const bookDiv = document.createElement("div");
+    bookDiv.classList.add("lib-book");
+
+    const bookTitle = document.createElement("h3");
+    bookTitle.textContent = myLibrary[i].title;
+    bookDiv.appendChild(bookTitle);
+    
+    const bookAuthor = document.createElement("p");
+    bookAuthor.textContent = `Authored by ${myLibrary[i].author}`;
+    bookDiv.appendChild(bookAuthor);
+
+    const haveIReadButton = document.createElement("button");
+    if (myLibrary[i].haveIRead == true) {
+        haveIReadButton.classList.add('have-read-this');
+        haveIReadButton.innerHTML = "Have Read This";
+    } else {
+        haveIReadButton.classList.add('not-read-yet');
+        haveIReadButton.innerHTML = "Not Read Yet";
+
+    }
+    haveIReadButton.addEventListener('click', e => {
+        toggleReadStatus(e.target);
+    });
+    bookDiv.appendChild(haveIReadButton);
+
+    const removeButton = document.createElement("button");
+    removeButton.classList.add("remove-book");
+    removeButton.innerHTML = "Remove";
+    removeButton.addEventListener("click", e => {
+        removeFromLibrary(e.target.parentElement);
+    });
+    bookDiv.appendChild(removeButton);
+
+    const bookContainer = document.querySelector("#book-container");
+    bookContainer.appendChild(bookDiv);
+}
+
+myLibrary.push(new Book('pythonnin in Java', 'blockchain enthusiast entrepreneur', 2, false));
+myLibrary.push(new Book('eateries in Lahore', 'emaciated person', 10000, false));
+myLibrary.push(new Book('Programming in Javascript', 'Linus Torwalds', 5000, true));
+for (let i = 0; i < myLibrary.length; i++) {
+    renderBook(i);
+}
