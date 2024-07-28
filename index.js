39834ec1472b0a64aee4894aaa4e8a87d59e8c74
MuhammadAbdullahWarraich <muhammadabdullah2016pk@gmail.com>
2024-07-28 17:53:36 +0500

    fix: remove old file

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const myLibrary = []
 
-function Book(title, author, noOfPages, haveIRead) {
-    this.title = title;
-    this.author = author;
-    this.noOfPages = noOfPages;
-    this.haveIRead = haveIRead;
-}
-
-Book.prototype.info = function() {
-    return `${this.title} by ${this.author}, ${this.noOfPages} pages, ${(this.haveIRead === true) ? "have read this" : "not read yet"}`
+class Book {
+    constructor(title, author, noOfPages, haveIRead) {
+        this.title = title;
+        this.author = author;
+        this.noOfPages = noOfPages;
+        this.haveIRead = haveIRead;
+    }
+    info() {
+        return `${this.title} by ${this.author}, ${this.noOfPages} pages, ${(this.haveIRead === true) ? "have read this" : "not read yet"}`
+    }
 }
 
 const openBookForm = document.querySelector('#open-book-form');
